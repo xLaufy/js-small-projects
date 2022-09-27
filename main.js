46576fba99ce357db3c1a3a6e622977b1dcb53c3
Miłosz Krawczyk <mixol456@gmail.com>
2022-09-27 23:12:34 +0200

    temp converter

--- main.js
+++ main.js
@@ -1,39 +1,55 @@
-const pass = document.querySelector('#password')
-const p = document.querySelector('.passinfo')
-const letters = /[a-z]/i
-const numbers = /[0-9]/
-const special = /[!@#$%^&*()]/
-const minValue = 10
+const converter = document.querySelector('#converter')
+const result = document.querySelector('.result')
+const convBtn = document.querySelector('.conv')
+const resetBtn = document.querySelector('.reset')
+const changeBtn = document.querySelector('.change')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+const value = converter.querySelector('.value')
 
-const showMsg = () => {
-	if (
-		pass.value.length > minValue &&
-		pass.value.match(letters) &&
-		pass.value.match(numbers) &&
-		pass.value.match(special)
-	) {
-		p.textContent = 'Masz bardzo dobre hasło!'
-		p.style.color = 'lime'
-	} else if (
-        pass.value.length > minValue && 
-        pass.value.match(letters) && 
-        pass.value.match(numbers)
-        ) {
-		p.textContent = 'Masz dobre hasło!'
-		p.style.color = 'gold'
+let fahrenheit
+let celsius
+
+const change = () => {
+	if (one.textContent === '°C') {
+		one.textContent = '°F'
+		two.textContent = '°C'
+		result.textContent = ''
+	} else if (one.textContent === '°F') {
+		one.textContent = '°C'
+		two.textContent = '°F'
+		result.textContent = ''
+	}
+}
+
+const tempC = () => {
+	celsius = (converter.value - 32) / 1.8
+	result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`
+	converter.value = ''
+}
+
+const tempF = () => {
+	fahrenheit = converter.value * 1.8 + 32
+	result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`
+	converter.value = ''
+}
+
+const conv = () => {
+	if (converter.value !== '') {
+		if (one.textContent === '°C') {
+			tempF()
+		} else {
+			tempC()
+		}
 	} else {
-        p.textContent = 'Masz słabe hasło!'
-		p.style.color = 'tomato'
-    }
+		result.textContent = 'Musisz podać jakąś wartość!'
+	}
+}
+const reset = () => {
+	result.textContent = ''
+	converter.value = ''
 }
 
-const checkPassword = () => {
-    if (pass.value !== '') {
-        showMsg()
-    } else {
-        p.textContent = 'Nie podałeś hasła...'
-		p.style.color = ''
-    }
-}
-
-pass.addEventListener('keyup', checkPassword)
+changeBtn.addEventListener('click', change)
+convBtn.addEventListener('click', conv)
+resetBtn.addEventListener('click', reset)
